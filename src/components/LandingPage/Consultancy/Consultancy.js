a503d1f486115e91f85ed1db9c39eaa93c21eaf3
mayankrajdsm <mayankraj.dsm@gmail.com>
2024-11-16 08:57:57 +0530

    merge remove unused tag

--- src/components/LandingPage/Consultancy/Consultancy.js
+++ src/components/LandingPage/Consultancy/Consultancy.js
@@ -27,8 +27,6 @@ const Consultancy = () => {
           <div className={classes.para}>
           <p className={classes.service2}>
               Conservation of Heritage Structures
-          <p className={classes.service2}>
-              Conservation of Heritage Structures
             </p>
             <hr />
             <p className={classes.service2}>Health Monitoring of Structures</p>
@@ -105,7 +103,6 @@ const Consultancy = () => {
         </div>
         <div className={classes.sec2}>
           <div>
-          <p className={classes.service1}>
           <p className={classes.service1}>
               Designing, Planning and Coordinating Architectural Services
             </p>
