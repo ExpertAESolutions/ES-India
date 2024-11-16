3c42916f3f0662c0411143b53fd4a8f43f08454d
mayankrajdsm <mayankraj.dsm@gmail.com>
2024-11-16 10:12:42 +0530

    iamge replacement client

--- src/components/LandingPage/OurClients/OurClients.js
+++ src/components/LandingPage/OurClients/OurClients.js
@@ -31,7 +31,7 @@ import IDPL from "../../../assets/Clients/IDPL.jpeg";
 import SamIndia from "../../../assets/Clients/Sam-India.jpeg";
 import PT from "../../../assets/Clients/PT.jpg";
 import TataSteel from "../../../assets/Clients/tata-steel.jpg";
-import Tristar from "../../../assets/Clients/Tristar1.png";
+import Tristar from "../../../assets/Clients/Tristar.png";
 import UPBC from "../../../assets/Clients/UPBC.jpg";
 
 const OurClients = () => {
